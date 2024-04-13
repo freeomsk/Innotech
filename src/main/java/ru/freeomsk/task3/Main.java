8f4a723a6bb7f34073818d48995a23d703970a46
freeomsk <freeomsk@yandex.ru>
2024-04-13 12:55:37 +0600

    Added task3.

---
+++ src/main/java/ru/freeomsk/task3/Main.java
@@ -0,0 +1,31 @@
+package ru.freeomsk.task3;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        // Создаем пул потоков с некоторым количеством рабочих потоков
+        SimpleThreadPool pool = new SimpleThreadPool(4);
+
+        // Добавляем задачи для выполнения в пул потоков
+        for (int i = 0; i < 10; i++) {
+            int taskNumber = i;
+            pool.execute(() -> {
+                System.out.println("Выполняется задача " + taskNumber + " в потоке " + Thread.currentThread().getName());
+                try {
+                    // Имитация продолжительной работы
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+        }
+
+        // Даем некоторое время на выполнение задач
+        Thread.sleep(5000);
+
+        // Инициируем остановку пула потоков. Это заставит пул перестать принимать новые задачи и корректно завершить текущие.
+        pool.shutdown();
+
+        // Ожидаем завершения всех задач в пуле перед выходом из программы
+        pool.awaitTermination();
+    }
+}
